d33c8a28955fd00906aa54059933e15e617e21ab
jisungbin <ji@sungb.in>
2021-09-17 19:00:53 +0900

    Feat: 지도 부분 UI 업데이트, 로직 수정

--- presentation/src/main/kotlin/team/mobileb/opgg/activity/chat/map/OffsetExtension.kt
+++ presentation/src/main/kotlin/team/mobileb/opgg/activity/chat/map/OffsetExtension.kt
@@ -2,4 +2,4 @@ package team.mobileb.opgg.activity.chat.map
 
 import androidx.compose.ui.geometry.Offset
 
-fun Offset.isVisible() = x != 0f && y != 0f
+fun Offset.isVisible() = x != 0.0f && y != 0.0f
